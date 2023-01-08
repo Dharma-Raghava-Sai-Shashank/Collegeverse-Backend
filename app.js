d5049ca29a05602495cfe867aaf9977c1dfdee0d
Dharma-Raghava-Sai-Shashank <104642011+Dharma-Raghava-Sai-Shashank@users.noreply.github.com>
2023-01-08 20:15:54 +0530

    db

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const User=require('./models/users')
+const User=require('./models/users')
 // const Post=require('./models/posts')
 // const Like=require('./models/likes')
 // const Comment=require('./models/comments')
@@ -8,18 +8,13 @@ const app = express()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
-const Mongoose = require('mongoose')
-Mongoose.set('strictQuery', false);
-const db ='mongodb+srv://saishashank:Arjun@cluster0.fevr2iu.mongodb.net/Collegeverse?retryWrites=true&w=majority'
-Mongoose.connect(db,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-        console.log("Connected to mongodb database")
-    }).catch((err)=> console.log(err))
-
 app.get('/',async(req,res)=>{
     return res.send("Server Started")
+})
+
+app.get('/about',async(req,res)=>{
+    var data=await User.find({})
+    return res.send(data)
 })
 
 app.listen(5000,()=>{
